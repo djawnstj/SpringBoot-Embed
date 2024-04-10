8ad46eaa74643e335f0aca686553f005e83ba8ba
junseo <djawnstj44@naver.com>
2024-04-10 17:34:27 +0900

    commit

---
+++ src/main/java/hello/boot/MySpringApplication.java
@@ -0,0 +1,42 @@
+package hello.boot;
+
+import hello.spring.HelloConfig;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import java.util.List;
+
+public class MySpringApplication {
+
+    public static void run(final Class<?> configClass, final String[] args) {
+        System.out.println("MySpringApplication.run args=" + List.of(args));
+
+        // 톰캣 설정
+        final Tomcat tomcat = new Tomcat();
+        final Connector connector = new Connector();
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+        // 스프링 컨테이너 설정
+        final AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(configClass);
+
+        // 스프링 MVC 디스패처 서블릿 생성, 스프링 컨테이너 연결
+        final DispatcherServlet dispatcher = new DispatcherServlet(appContext);
+
+        // 디스패처 서블릿 등록
+        final Context context = tomcat.addContext("", "/");
+        tomcat.addServlet("", "dispatcher", dispatcher);
+        context.addServletMappingDecoded("/", "dispatcher");
+
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
